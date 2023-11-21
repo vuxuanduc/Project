6ff2ec971eb335dc7d11dad86563b53c927c91a9
Tên của bạn <email@xample.com>
2023-11-21 07:48:07 +0700

    Update code

--- model/status.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // Quản lý trạng thái ;
-    function getStatus() {
-        $conn = connectDB() ;
-        $sql = "SELECT * FROM `status`" ;
-        $result = $conn -> query($sql) -> fetchAll() ;
-        return $result ;
-    }
-?>
